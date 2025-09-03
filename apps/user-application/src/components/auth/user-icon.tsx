2d1f772567caf3d611cfe14fc6e4e451019153b2
Alazar Genene <alazar.genene@gmail.com>
2025-09-03 13:53:56 -0400

    fix: get out when the user logs out

--- apps/user-application/src/components/auth/user-icon.tsx
+++ apps/user-application/src/components/auth/user-icon.tsx
@@ -10,6 +10,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { LogOut, User } from "lucide-react";
 import { useState } from "react";
 import { authClient } from "./client";
+import { useNavigate } from "@tanstack/react-router";
 
 // Mock authClient with dummy data
 
@@ -21,13 +22,13 @@ type UserProfilePopupProps = {
 
 function UserProfilePopup({ data, children }: UserProfilePopupProps) {
   const [loading, setLoading] = useState(false);
-
+  const nav = useNavigate();
   const handleLogout = async () => {
     setLoading(true);
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
-          window.location.reload();
+          nav({to: "/"})
         },
       },
     });
